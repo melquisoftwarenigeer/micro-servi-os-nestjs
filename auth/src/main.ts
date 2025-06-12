74b67d28f74f32deeddad18ab4895c79af8a4525
melquisoftwarenigeer <melquij5pro@gmail.com>
2025-06-11 23:34:58 -0300

    Fix(environment) Docker porter dinamic

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
+  const isDocker = fs.existsSync('/.dockerenv');
+  const AUTH_PORT = isDocker ? (process.env.AUTH_PORT || 3000) : 3001;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());  // Aplica o ValidationPipe globalmente
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(AUTH_PORT);
 }
-bootstrap();
+bootstrap(); 
